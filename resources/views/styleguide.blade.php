f4c22d11d5b35ebec63c3e71a5c70f76599204e2
Manuel <crowberry23@gmail.com>
2018-01-17 14:58:55 +0100

    start styleguide

---
+++ resources/views/styleguide.blade.php
@@ -0,0 +1,65 @@
+@extends('welcome')
+
+
+    <h1>Styleguide</h1>
+    <section>
+        <h2>Logo</h2>
+    </section>
+
+    <section>
+        <h2>Fonts</h2>
+    </section>
+
+    <section>
+        <h2>Couleurs</h2>
+        <h3>Marque</h3>
+        <ul>
+            <li>Color 1 : <span></span></li>
+            <li>Color 2 : <span></span></li>
+            <li>Color 3 : <span></span></li>
+        </ul>
+
+        <h3>Gris</h3>
+        <ul>
+            <li>Gris 1 : <span></span></li>
+            <li>Gris 2 : <span></span></li>
+            <li>Gris 3 : <span></span></li>
+            <li>Gris 4 : <span></span></li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Boutons</h2>
+        <ul>
+            <li>
+                <a href="#">Btn principal 1</a>
+                <a href="#">Btn principal 2</a>
+            </li>
+            <li>
+                <a href="#">Btn secondaire 1</a>
+                <a href="#">Btn secondaire 2</a>
+            </li>
+            <li>
+                <a href="#">Btn tertiaire 1</a>
+                <a href="#">Btn tertiaire 2</a>
+            </li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Avatars - icones</h2>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Badges</h2>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </section>
