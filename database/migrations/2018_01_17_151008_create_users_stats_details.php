15f5fe9b1a433f97aaf424f03286e357187de800
Guillaume Griggo <delphine.g@wegravit.com>
2018-01-17 16:24:39 +0100

    Migrations users_stats et users_stats_details

---
+++ database/migrations/2018_01_17_151008_create_users_stats_details.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersStatsDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_stats_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('game_id')->unsigned();
+            $table->foreign('game_id')
+                ->references('id')
+                ->on('games')
+                ->onDelete('cascade');
+            $table->integer('user_stats_id')->unsigned();
+            $table->foreign('user_stats_id')
+                ->references('id')
+                ->on('users_stats')
+                ->onDelete('cascade');
+            $table->boolean('won');
+            $table->integer('total_points')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_stats_details');
+    }
+}
