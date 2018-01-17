6144ff6513a07c3610463cea8e9facf112f57c94
delphine.g <delphine.gouwy@gmail.com>
2018-01-17 18:18:27 +0100

    Avancée connexion
    Reste statu connection et récupération image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-signin-scope" content="profile email">
+        <meta name="google-signin-client_id" content="996656444072-6irn35okejaesihhtgoi8hnv1d30i8et.apps.googleusercontent.com">
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
 
         <title>Laravel</title>
 
@@ -15,16 +18,67 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
                 <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+                        <div class="g-signin2" id="signin-button" onclick="initClient()" data-theme="dark"></div>
+                            <input type="button"  value="Logout" onclick="revokeAllScopes()" />
+    <script>
+    /**
+     * The Sign-In client object.
+     */
+    var auth2;
+
+    /**
+     * Initializes the Sign-In client.
+     */
+    function initClient() {
+      console.log("init");
+        gapi.load('auth2', function(){
+            /**
+             * Retrieve the singleton for the GoogleAuth library and set up the
+             * client.
+             */
+            auth2 = gapi.auth2.init({
+                client_id: '996656444072-6irn35okejaesihhtgoi8hnv1d30i8et.apps.googleusercontent.com'
+            });
+            console.log(auth2);
+            // Attach the click handler to the sign-in button
+            var button = $('#signin-button');
+            console.log(button);
+            auth2.attachClickHandler(button, {}, onSuccess, onFailure);
+        });
+    };
+
+    /**
+     * Handle successful sign-ins.
+     */
+    function onSuccess(user) {
+      console.log("success");
+        console.log('Signed in as ' + user.getBasicProfile().getName());
+     };
+
+    /**
+     * Handle sign-in failures.
+     */
+    function onFailure(error) {
+      console.log("error");
+        console.log(error);
+    };
+
+      function revokeAllScopes() {
+        console.log(gapi.auth2.getAuthInstance().isSignedIn.Ab);
+        gapi.auth2.getAuthInstance().disconnect();
+      };
+
+      function isConnect() {
+        console.log(gapi.auth2.getAuthInstance().isSignedIn.Ab);
+        isCo = gapi.auth2.getAuthInstance().isSignedIn.Ab
+        return isCo;
+      };
+      console.log(isConnect());
+
+    </script>
+
                 </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
