ce459bc15cacabb66236a13a0c3c23daa4e69e6c
Remi <remi@wegravit.com>
2018-01-17 16:43:23 +0100

    Intégration api slack
    - ajout sdk slack
    - création fonction permettant l'envoi de message à une chaine via le bot "Motton"

---
+++ config/slack.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	"preprod" => [
+		'token' => 'xoxp-298992438033-300400274806-301274944247-8d9fc6a4761bce6086bab38850bace5e',
+        'team' => 'perlinpinpin',
+        'username' => 'Motton',
+        'icon' => 'ICON', // Auto detects if it's an icon_url or icon_emoji
+        'parse' => '', // __construct function in Client.php calls for the parse parameter 
+	]
+];
