02e6e7aa1786cf00905f5be2f64cb147e27b2bcb
remi@wegravit.com <remi@wegravit.com>
2018-01-17 17:08:45 +0100

    avancement modèle

--- database/migrations/2018_01_17_143037_create_games_table.php
+++ database/migrations/2018_01_17_143037_create_games_table.php
@@ -16,9 +16,6 @@ class CreateGamesTable extends Migration
       Schema::create('games', function (Blueprint $table) {
           $table->increments('id');
           $table->string('name');
-          $table->integer('admin_id')->unsigned();
-          $table->foreign('admin_id')->references('id')->on('users')
-              ->onDelete('cascade');
           $table->timestamps();
       });
     }
