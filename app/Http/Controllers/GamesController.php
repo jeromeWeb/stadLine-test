8cdea938ee5d479fb892f7f5b4a85cd52c8b46b1
karimchachi <karim.chachi@gmail.com>
2018-01-18 12:26:26 +0100

    gamesController

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -7,13 +7,20 @@ use TIE\
 
 class GamesController extends Controller
 {
-    public function point(){
+    public function bet(Resquest $request){
 
-    	DB::table('rounduser')->insert([
-         'bet' => $bet,
-
+    	DB::table('round_user')->insert([
+        'round_id' => $request->round_id,
+        'user_id' => $request->user_id,
+        'bet' => $request->bet,
        ]);
+    }
 
 
+    public function result(Request $request){
+
+    	DB::table('round_user')->insert([
+        'result' => $request->result,
+       ]);
     }
 }
