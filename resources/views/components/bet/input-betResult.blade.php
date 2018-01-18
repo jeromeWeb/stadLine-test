a76d877ad5a0ff92922f7b3d5f9b607b4d8bdc12
Manuel <crowberry23@gmail.com>
2018-01-18 18:09:06 +0100

    pre sync master

--- resources/views/components/bet/input-betResult.blade.php
+++ resources/views/components/bet/input-betResult.blade.php
@@ -4,18 +4,18 @@
 		<p class="h3">Tour {{$round->nb_round}} - {{$round->nb_card}} Cartes</p>
 	</header>
 </div>
-<section data-stape="result" class="col-xs-12 col-sm-6 col-sm-push-3 bet-result" id="bet-inputs">
+<section data-stape="result" class="col-xs-12 col-sm-6 col-sm-push-3" id="bet-inputs">
 @php $i = 1; @endphp
 @foreach($game->players->pluck('user') as $player)
 	<div class="players clearfix">
 		<div class="pull-left">
-			@include('components/avatar', [$j => $j])
+			@include('components/avatar')
 		</div>
 		<div class="pull-left">
 			<label for="player{{$i}}">{{$player->pseudo}}</label>
 			<p class="txt-brand"><span class="fw-bold">0</span> point</p>
 		</div>
-		<div class="pull-right">
+		<div class="pull-right bet-result">
 			<div class="pull-left">
 				<label>Prédit</label>
 				<p>{{$bets[$player->id]}}</p>
