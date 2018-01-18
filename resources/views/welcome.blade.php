db3dbfb314cfbaa50dd006b7483541381e2e0ad1
delphine.g <delphine.gouwy@gmail.com>
2018-01-18 09:34:18 +0100

    Récupération statu connexion en cours

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <meta name="google-signin-scope" content="profile email">
         <meta name="google-signin-client_id" content="996656444072-6irn35okejaesihhtgoi8hnv1d30i8et.apps.googleusercontent.com">
         <script src="https://apis.google.com/js/platform.js" async defer></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
         <title>Laravel</title>
 
@@ -19,66 +20,9 @@
     <body>
         <div class="flex-center position-ref full-height">
                 <div class="top-right links">
-                        <div class="g-signin2" id="signin-button" onclick="initClient()" data-theme="dark"></div>
-                            <input type="button"  value="Logout" onclick="revokeAllScopes()" />
-    <script>
-    /**
-     * The Sign-In client object.
-     */
-    var auth2;
-
-    /**
-     * Initializes the Sign-In client.
-     */
-    function initClient() {
-      console.log("init");
-        gapi.load('auth2', function(){
-            /**
-             * Retrieve the singleton for the GoogleAuth library and set up the
-             * client.
-             */
-            auth2 = gapi.auth2.init({
-                client_id: '996656444072-6irn35okejaesihhtgoi8hnv1d30i8et.apps.googleusercontent.com'
-            });
-            console.log(auth2);
-            // Attach the click handler to the sign-in button
-            var button = $('#signin-button');
-            console.log(button);
-            auth2.attachClickHandler(button, {}, onSuccess, onFailure);
-        });
-    };
-
-    /**
-     * Handle successful sign-ins.
-     */
-    function onSuccess(user) {
-      console.log("success");
-        console.log('Signed in as ' + user.getBasicProfile().getName());
-     };
-
-    /**
-     * Handle sign-in failures.
-     */
-    function onFailure(error) {
-      console.log("error");
-        console.log(error);
-    };
-
-      function revokeAllScopes() {
-        console.log(gapi.auth2.getAuthInstance().isSignedIn.Ab);
-        gapi.auth2.getAuthInstance().disconnect();
-      };
-
-      function isConnect() {
-        console.log(gapi.auth2.getAuthInstance().isSignedIn.Ab);
-        isCo = gapi.auth2.getAuthInstance().isSignedIn.Ab
-        return isCo;
-      };
-      console.log(isConnect());
-
-    </script>
-
-                </div>
+                        <div class="g-signin2" id="signin-button" data-theme="dark"></div>
+                            <input type="button" id="logout"  value="Logout" />
+        </div>
 
             <div class="content">
                 <div class="title m-b-md">
@@ -96,5 +40,69 @@
         </div>
 
          <script src="{{ URL::to('js/app.js') }}"></script>
+         @push('footer_js_variables')
+            var isConnect = isConnect();
+         @endpush
+         <script>
+
+         /**
+          * The Sign-In client object.
+          */
+         var auth2;
+
+         /**
+          * Initializes the Sign-In client.
+          */
+         $("#signin-button").on("click", function() {
+           console.log("init");
+             gapi.load('auth2', function(){
+                 /**
+                  * Retrieve the singleton for the GoogleAuth library and set up the
+                  * client.
+                  */
+                 auth2 = gapi.auth2.init({
+                     client_id: '996656444072-6irn35okejaesihhtgoi8hnv1d30i8et.apps.googleusercontent.com'
+                 });
+                 console.log(auth2);
+                 // Attach the click handler to the sign-in button
+                 var button = $('#signin-button');
+                 console.log(button);
+                 auth2.attachClickHandler(button, {}, onSuccess, onFailure);
+             });
+         });
+
+         /**
+          * Handle successful sign-ins.
+          */
+         function onSuccess(user) {
+           console.log("success");
+             console.log('Signed in as ' + user.getBasicProfile().getName());
+          };
+
+         /**
+          * Handle sign-in failures.
+          */
+         function onFailure(error) {
+           console.log("error");
+             console.log(error);
+         };
+
+           $("#logout").on("click", function(){
+             console.log(gapi.auth2.getAuthInstance().isSignedIn.Ab);
+             gapi.auth2.getAuthInstance().disconnect();
+           });
+
+             console.log("plop");
+             function isConnect() {
+               console.log(gapi.auth2.getAuthInstance().isSignedIn.Ab);
+              var  isCo = gapi.auth2.getAuthInstance().isSignedIn.Ab
+               return isCo;
+             };
+             console.log(isConnect());
+
+
+         </script>
+
+
     </body>
 </html>
