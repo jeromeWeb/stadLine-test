575cfbf51765c2fc206fd66f28753f510040e741
karimchachi <karim.chachi@gmail.com>
2018-01-18 09:58:40 +0100

    push

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use TIE\
 
 class GamesController extends Controller
 {
-    //
+    public function point(){
+
+    	DB::table('rounduser')->insert([
+         'bet' => $bet,
+
+       ]);
+
+
+    }
 }
