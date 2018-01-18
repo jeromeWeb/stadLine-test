36d4061004500976c42bfc4234722447b0ea4bc1
Remi <remi@wegravit.com>
2018-01-18 09:25:04 +0100

    Config prod/preprod
    Mise en place système de choix automatique des configs

--- config/slack.php
+++ config/slack.php
@@ -4,7 +4,14 @@ return [
 	"preprod" => [
 		'token' => 'xoxp-298992438033-300400274806-301274944247-8d9fc6a4761bce6086bab38850bace5e',
         'team' => 'perlinpinpin',
-        'username' => 'Motton',
+        'username' => 'Motton-preprod',
+        'icon' => 'ICON', // Auto detects if it's an icon_url or icon_emoji
+        'parse' => '', // __construct function in Client.php calls for the parse parameter 
+	],
+	"prod" => [
+		'token' => 'xoxp-298992438033-300400274806-301274944247-8d9fc6a4761bce6086bab38850bace5e',
+        'team' => 'perlinpinpin',
+        'username' => 'Motton-prod',
         'icon' => 'ICON', // Auto detects if it's an icon_url or icon_emoji
         'parse' => '', // __construct function in Client.php calls for the parse parameter 
 	]
